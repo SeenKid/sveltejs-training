b435ebe13ecdee4137d0246ca9ee15baa6a13db3
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-23 08:50:24 +0100

    Create helloworld_colors.svelte

---
+++ helloworld_colors.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = 'Yann';
+	let color ="cyan";
+</script>
+
+<h1>Hello {name}!</h1>
+
+<label for="name">Name</label>
+
+<input id="name" name="name" bind:value={name}/>
+
+<label for="color">color</label>
+<input type="color" name="color" id="color" bind:value={color} />
+
+<h1 style="color:{color}">
+	Hello World
+</h1>
+
+<p>
+	La couleur choisie est {color}
+</p>
