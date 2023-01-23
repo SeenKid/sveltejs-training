d5a5a5dccffe8dadb80935178d073af601e6fcbd
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-23 09:44:55 +0100

    Create app.svelte

---
+++ clock/app.svelte
@@ -0,0 +1,18 @@
+<script>
+	// import component 
+	import Clock from "./Clock.svelte";
+	
+	// Variables
+	let name = 'Clock with Svelte';
+	let color1 = "blue";
+	let color2 = "green";
+	let color3 = "cyan"
+</script>
+
+<h1>{name}!</h1>
+
+<Clock color="{color1}"/>
+<!--
+<Clock color="{color2}"/>
+<Clock color="{color3}"/>
+-->
