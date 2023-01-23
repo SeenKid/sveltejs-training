58022283a1106b838e89759eb8e8ae069d02262d
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-23 09:45:08 +0100

    Create Clock.svelte

---
+++ clock/Clock.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let color = "blue";
+	import { onMount} from 'svelte';
+	
+	// variables
+	const ms = 1000;
+	let time = new Date();
+	$: hours = time.getHours();
+	$: minutes = time.getMinutes();
+	$: seconds = time.getSeconds();
+	
+	onMount(() => {
+		const interval = setInterval (() => {
+			time = new Date();
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	});
+	
+</script>
+
+<p style="color:{color}">
+	{hours}:{minutes}:{seconds}
+</p>
