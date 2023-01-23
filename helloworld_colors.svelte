7bf90ce9b5a256d95f9510b47040c6ce1dfd52c2
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-23 09:10:43 +0100

    added comments

--- helloworld_colors.svelte
+++ helloworld_colors.svelte
@@ -1,8 +1,10 @@
 <script>
+	// Variables 
 	let name = 'Yann';
 	let color ="cyan";
 	let isUpper = false;
 	$: upper = "Hello "+name +"!";
+	$: txtFinal = isUpper ? upper.toUpperCase() : upper;
 </script>
 
 <h1>Hello {name}!</h1>
@@ -15,12 +17,20 @@
 <input type="color" name="color" id="color" bind:value={color} />
 
 <br>
+<!-- checkbox to enable uppercases -->
 <input type="checkbox" name="upper" id="upper" bind:checked={isUpper}/> Uppercase
 
+<!-- h1 modifying -->
 <h1 style="color:{color}">
 	{isUpper ? upper.toUpperCase() : upper}
 </h1>
 
+<!-- same shit but with variable -->
+<h1 style="color:{color}">
+	{txtFinal}
+</h1>
+
+<!-- say the choosed color & say if uppercase is enabled -->
 <p>
 	La couleur choisie est {color}, la case à cocher est sur {isUpper}
 </p>
