554dc0969c035a39dea535f90b75b05a02ad860a
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-30 11:09:24 +0100

    Create +page.svelte

---
+++ NumberStats/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let numbers = [];
+    let currentValue = 0;
+
+    function addNumber() {
+        numbers.push(currentValue);
+        numbers = [...numbers, currentValue];
+
+    console.log(numbers);
+    }
+
+    $: size = numbers.length;
+    $: min = Math.min(...numbers);
+    $: max = Math.max(...numbers);
+    $: avg = numbers.reduce((x,y ) => x + y, 0) / size;
+</script>
+
