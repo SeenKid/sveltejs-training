3751ac9f9ac98a12650358d2ad02497e9287126f
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-30 09:36:05 +0100

    Create +page.svelte

---
+++ event/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let i = 0;
+    function incrementCounter() {
+        ++i;
+        console.log({i});
+    }
+</script>
+
+<br/>
+
+<button on:click={incrementCounter}>Cliquez</button>
+
+<p> nombre de clicks sur le bouton : {i}</p>
