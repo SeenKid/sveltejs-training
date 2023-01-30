9adae0a5705afcb63e218cd3d7910acffdb88b64
Yann <30565751+SeenKid@users.noreply.github.com>
2023-01-30 10:17:11 +0100

    Update +page.svelte

--- event/+page.svelte
+++ event/+page.svelte
@@ -2,11 +2,12 @@
     let i = 0;
     function incrementCounter() {
         ++i;
-        console.log({i});
+        console.log("la valeur du compteur est de : " + i);
     }
 </script>
 
 <br/>
+<h1> Compteur de clicks </h1>
 
 <button on:click={incrementCounter}>Cliquez</button>
 
