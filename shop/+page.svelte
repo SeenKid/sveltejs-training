8271f939a6ff065537969cc8398157b8eb9429d7
Yann <30565751+SeenKid@users.noreply.github.com>
2023-02-06 10:22:58 +0100

    Create +page.svelte

---
+++ shop/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data;
+    const { product } = data;
+    //const {title, description, price} = data.product;
+    //const title = data.title;
+</script>
+
+<h1>{product.title}</h1>
+<p>{data.product.description}</p>
+<p>{product.price} CHF</p>
