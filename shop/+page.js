a4c9364a3977fab457a13da5e24aa8a147375226
Yann <30565751+SeenKid@users.noreply.github.com>
2023-02-06 10:22:44 +0100

    Create +page.js

---
+++ shop/+page.js
@@ -0,0 +1,10 @@
+/** @type {import('./$types').Pageload} **/
+export async function load() {
+    return {
+        product : {
+            title : 'HP ProLiant DL380 Gen9 2U 4x 3.5" (LFF)',
+            description : "Part of the world's best selling server range, the DL380 Gen9 continues to deliver. Utilizing the late generation E5-2600 V3 & V4 Xeon series and DDR4 Smart Memory the DL380 has the ability to adapt and expand to fit any environment featuring up to 3TB High Availability DDR4 RAM and two Twenty Two (22) Core Xeon processors, this cornerstone of datacentre's excels in Virtualization, Big data, Data Analytics and Enterprise Resource Planning (ERP) Environments.",
+            price: 1400
+        }
+    }
+}
